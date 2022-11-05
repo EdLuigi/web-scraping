37127a64e8cdac02bb6c325cd09cf61c9b4d4557
Luigi <luigi.ciuffi@hotmail.com>
2022-11-05 08:05:50 -0300

    código scraping pronto

--- web_scraping/spiders/gov.py
+++ web_scraping/spiders/gov.py
@@ -3,8 +3,21 @@ import scrapy
 
 class GovSpider(scrapy.Spider):
     name = 'gov'
-    allowed_domains = ['www.gov.br']
-    start_urls = ['http://www.gov.br/']
+    # allowed_domains = ['www.gov.br']
+    start_urls = ['https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude']
 
     def parse(self, response):
-        pass
+        for link in response.css('.internal-link'):
+            
+            titulo = link.css('::text').get()
+            arquivo = response.urljoin(
+                    link.css('::attr(href)').get()
+            )
+            
+            if titulo[:5] == 'Anexo':
+                print('>>>>',titulo)
+                print('>>>>',arquivo)
+                yield{
+                    'Title':titulo,
+                    'file_urls':[arquivo]
+                }
